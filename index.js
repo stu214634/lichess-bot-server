a6a196948d0d40d1183c8559ff2745aff6fd3368
Jan Busse <jan-patric.busse@adesso.de>
2022-04-06 15:53:48 +0200

    work

--- index.js
+++ index.js
@@ -13,66 +13,81 @@ async function stream(url, callback) {
         responseType: "stream",
     });
 
-    response.data.pipe(ndjson.parse()).on("data", callback).on("error", console.error);
+    return new Promise((resolve, reject) => {
+        response.data.pipe(ndjson.parse())
+            .on("data", callback)
+            .on("end", resolve)
+            .on("error", reject);
+    });
 }
 
 async function main() {
     console.log("Started lichess server");
 
-    stream("/api/stream/event", event => {
-        console.log(event);
-        const f = ({
-            challenge: () => {
-                console.log("Accepting challenge from " + event.challenge.challenger.name);
-                axios.post(`/api/challenge/${event.challenge.id}/accept`);
-            },
-            gameStart: async () => {
-                console.log("Game started with " + event.game.opponent.username);
-                const me = event.game.color[0];
-                const engine = childProcess.spawn("main.exe", {
-                    stdio: ["pipe", "pipe"],
-                });
+    while (true) {
+        await stream("/api/stream/event", event => {
+            console.log(event);
+            const f = ({
+                challenge: async () => {
+                    console.log("Accepting challenge from " + event.challenge.challenger.name);
+                    try {
+                        await axios.post(`/api/challenge/${event.challenge.id}/accept`);
+                    } catch (err) {
+                        console.error(err);
+                    }
+                },
+                gameStart: async () => {
+                    console.log("Game started with " + event.game.opponent.username);
+                    const me = event.game.color[0];
+                    const engine = childProcess.spawn("main.exe", {
+                        stdio: ["pipe", "pipe"],
+                    });
 
-                stream(`/api/bot/game/stream/${event.game.gameId}`, gameState => {
-                    console.log(gameState);
+                    stream(`/api/bot/game/stream/${event.game.gameId}`, gameState => {
+                        console.debug(gameState);
 
-                    gameState = gameState.state || gameState;
-                    if (gameState.status !== "started") return;
+                        gameState = gameState.state || gameState;
+                        if (gameState.status !== "started") return;
 
-                    const chess = new Chess();
+                        const chess = new Chess();
 
-                    const moves = gameState.moves;
-                    if (moves) {
-                        for (const move of moves.split(" ")) {
-                            if (!chess.move(move, { sloppy: true })) {
-                                console.log("Unable to parse move: " + move);
+                        const moves = gameState.moves;
+                        if (moves) {
+                            for (const move of moves.split(" ")) {
+                                if (!chess.move(move, { sloppy: true })) {
+                                    console.log("Unable to parse move: " + move);
+                                }
+                            }
+                        }
+
+                        if (chess.turn() === me) {
+                            console.log("Asking engine for move...");
+                            engine.stdin.write("position fen " + chess.fen() + "\n");
+                            engine.stdin.write("go 5\n");
+                        }
+                    });
+
+                    for await (let line of chunksToLinesAsync(engine.stdout)) {
+                        line = line.trim();
+                        console.log("Engine: " + line);
+                        if (line.startsWith("bestmove ")) {
+                            const move = line.substring("bestmove ".length);
+                            console.log("Sending move: " + move);
+
+                            try {
+                                await axios.post(`/api/bot/game/${event.game.gameId}/move/${move}`);
+                            } catch (err) {
+                                console.error(err);
                             }
                         }
                     }
-
-                    console.log(chess.ascii());
-
-                    if (chess.turn() === me) {
-                        engine.stdin.write("position fen " + chess.fen() + "\n");
-                        engine.stdin.write("go 4\n");
-                    }
-                });
-
-                for await (let line of chunksToLinesAsync(engine.stdout)) {
-                    line = line.trim();
-                    console.log("Engine: " + line);
-                    if (line.startsWith("bestmove ")) {
-                        const move = line.substring("bestmove ".length);
-                        console.log("Sending move: " + move);
-                        axios.post(`/api/bot/game/${event.game.gameId}/move/${move}`);
-                    }
                 }
-            }
-        })[event.type];
+            })[event.type];
 
-        if (f) f();
-        else console.log("Unknown event: " + event.type);
-    });
+            if (f) f();
+            else console.log("Unknown event: " + event.type);
+        });
+    }
 }
 
 main();
